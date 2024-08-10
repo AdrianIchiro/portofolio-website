090beb91d65a0e89faf03794fea68b86b1c621b5
Adrian <118010000+AdrianIchiro@users.noreply.github.com>
2024-08-11 01:15:23 +0700

    Update ProjectSection.js

--- src/app/components/ProjectSection.js
+++ src/app/components/ProjectSection.js
@@ -6,7 +6,7 @@ export default function ProjectSection() {
             <h2 className="text-center font-semibold text-3xl mb-10">My Project</h2>
             <div className="grid lg:grid-cols-2 md:gap-12">
                 <ProjectCard urlImg={'img/project-1.png'} title={'Book Store'} desc={'Description'} url={'https://library-it-website.vercel.app/'}/>
-                <ProjectCard urlImg={'https://www.telkomsel.com/sites/default/files/2023-08/aug-3-01.png'} title={'CoffeJava'} desc={'Online shop for coffee'} url={'https://website-coffejava-ahas9wwp8-adrians-projects-232e110a.vercel.app/'} />
+                <ProjectCard urlImg={'img/project-2.png'} title={'CoffeJava'} desc={'Online shop for coffee'} url={'https://website-coffejava-ahas9wwp8-adrians-projects-232e110a.vercel.app/'} />
             </div>
         </section>
     )
